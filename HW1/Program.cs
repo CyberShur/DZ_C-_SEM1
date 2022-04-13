43ddf4258f77138d9ee2e54a15899c610df9673d
Aleksandr <aldrobakha@gmail.com>
2022-04-13 16:59:39 +0300

    Решена 1 задача

--- HW1/Program.cs
+++ HW1/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа. 456 -> 6 782 -> 2 918 -> 8
+
+Console.Write("Введите трёхзначное число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int Number3 = Number % 10;
+
+Console.WriteLine(Number3);
