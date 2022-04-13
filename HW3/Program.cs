f2af13cc504b1206efb77c0e29f1c45a26f04354
Aleksandr <aldrobakha@gmail.com>
2022-04-13 17:04:43 +0300

    Решена 3 задача

--- HW3/Program.cs
+++ HW3/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите число A: ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число C: ");
+int NumberC = Convert.ToInt32(Console.ReadLine());
+
+int max = NumberA;
+
+if (NumberA > max) max = NumberA;
+if (NumberB > max) max = NumberB;
+if (NumberC > max) max = NumberC;
+
+Console.Write("Максимальное число = ");
+Console.WriteLine(max);
