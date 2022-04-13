ffae9f1d29df9dfb2ed48166000402a6135e7eae
Aleksandr <aldrobakha@gmail.com>
2022-04-13 17:13:23 +0300

    Решена 5 задача

---
+++ HW5/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+
+while (i <= Number)
+{
+    if (i % 2 == 0) Console.Write(" " + i + ", ");
+    i++;
+}
